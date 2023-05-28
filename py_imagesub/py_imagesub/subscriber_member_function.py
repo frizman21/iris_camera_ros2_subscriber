67bea0436c516354ce170750119b7b0c8bd3318b
Mike Frizzell <mfrizzell@thealbersgroup.com>
2023-05-28 16:21:10 -0500

    in theory, I'm searching for aruco markers

--- py_imagesub/py_imagesub/subscriber_member_function.py
+++ py_imagesub/py_imagesub/subscriber_member_function.py
@@ -12,12 +12,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+import socket
+# import exceptions
+import math
+import argparse
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2 # OpenCV library
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
+
+id_to_find = 72
+marker_size = 19 #cm
+takeoff_height = 8
+
+# aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
+# parameters = aruco.DetectorParameters_create()
+
+dictionary = cv2.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
+parameters =  cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+
+
+##Camera
+horizontal_res = 640
+vertical_res = 480
+
+horizontal_fov = 62.2 * (math.pi / 180 ) ##Pi cam V1: 53.5 V2: 62.2
+vertical_fov = 48.8 * (math.pi / 180)    ##Pi cam V1: 41.41 V2: 48.8
+
+calib_path="/home/mike/ros2_opencv_lander/py_imagesub/"
+cameraMatrix   = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
+cameraDistortion   = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
 
 class MinimalSubscriber(Node):
 
@@ -30,9 +63,28 @@ class MinimalSubscriber(Node):
             10)
         self.subscription  # prevent unused variable warning
 
+        self.br = CvBridge()
+
     def listener_callback(self, msg):
         self.get_logger().info('I heard:')
 
+        current_frame = self.br.imgmsg_to_cv2(msg)
+
+        gray_img = cv2.cvtColor(current_frame,cv2.COLOR_BGR2GRAY)
+
+        # corners, ids, rejected = aruco.detectMarkers(image=gray_img,dictionary=aruco_dict,parameters=parameters)
+        
+        markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(gray_img)
+        
+        if markerIds is not None:
+            self.get_logger().info('   Found Something!')
+        else:
+            self.get_logger().info('   Nothing.')
+
+        cv2.imshow("camera", gray_img)
+        cv2.waitKey(1)
+        
+        
 
 def main(args=None):
     rclpy.init(args=args)
